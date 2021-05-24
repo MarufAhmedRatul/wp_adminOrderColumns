6059c8f895af0b784a6dd0674df7d29ae8d431ed
Maruf Ahmed <ahmedratul217@gmail.com>
2021-05-25 01:15:28 +0600

    add more post meta fields

--- includes/order-columns-main.php
+++ includes/order-columns-main.php
@@ -18,18 +18,42 @@ if( !class_exists( 'Order_Columns_Main' ) ) {
 			// Get "Delivery Type" from meta data (not item meta data)
 			$updated_delivery_type = $order->get_meta('_delivery_type');
 			$updated_advance_payment = $order->get_meta('_advance_payment');
+			$updated_call_status = $order->get_meta('_call_status');
 		
 			// Replace "Delivery Type" value by the meta data if it exist
 			$deliveryType = $updated_delivery_type ? $updated_delivery_type : ( isset($item_value) ? $item_value : '');
 			$advancePayment = $updated_advance_payment ? $updated_advance_payment : ( isset($item_value) ? $item_value : '');
+			$CallStatus = $updated_call_status ? $updated_call_status : ( isset($item_value) ? $item_value : '');
 		
 			// Display the custom editable field
-			woocommerce_wp_text_input( 
+			woocommerce_wp_select( 
 				array(
 					'id'            => 'delivery_type',
 					'label'         => __("Delivery Type:", "woocommerce"),
 					'value'         => $deliveryType,
 					'wrapper_class' => 'form-field-wide',
+					'options' => array(
+						''        => __( 'Select One', 'woocommerce' ),
+						'sundarban'   => __( 'Sundarban', 'woocommerce' ),
+						'pathao'   => __( 'Pathao', 'woocommerce' ),
+						'redx' => __( 'Redx', 'woocommerce' ),
+						'other' => __( 'Other', 'woocommerce' ),
+					)
+				)
+			);
+			
+			woocommerce_wp_select( 
+				array(
+					'id'            => 'call_status',
+					'label'         => __("Call Status:", "woocommerce"),
+					'value'         => $CallStatus,
+					'wrapper_class' => 'form-field-wide',
+					'options' => array(
+						''        => __( 'Select One', 'woocommerce' ),
+						'yes'   => __( 'Yes', 'woocommerce' ),
+						'no'   => __( 'No', 'woocommerce' ),
+						'wfc' => __( 'Waiting for Confirmation', 'woocommerce' )
+					)
 				)
 			);
 			woocommerce_wp_text_input( 
@@ -37,22 +61,25 @@ if( !class_exists( 'Order_Columns_Main' ) ) {
 					'id'            => 'advance_payment',
 					'label'         => __("Advance Payment:", "woocommerce"),
 					'value'         => $advancePayment,
-					'wrapper_class' => 'form-field-wide',
-				)
+					'wrapper_class' => 'form-field-wide'
+				)				
 			);
 		}
 
+
 		// Save the custom editable field value as order meta data and update order item meta data	
 		function save_order_custom_field_meta_data( $post_id, $post ){
-			if( isset( $_POST[ 'delivery_type' ] ) ||  isset( $_POST[ 'advance_payment' ] )){
-				// Save "Delivery Type" as order meta data
+			if( isset( $_POST[ 'delivery_type' ] ) ||  isset( $_POST[ 'advance_payment' ] ) || isset( $_POST[ 'call_status' ] )){
+				// Save "Delivery Type" as order meta data 
 				update_post_meta( $post_id, '_delivery_type', sanitize_text_field( $_POST[ 'delivery_type' ] ) );
 				update_post_meta( $post_id, '_advance_payment', sanitize_text_field( $_POST[ 'advance_payment' ] ) );
+				update_post_meta( $post_id, '_call_status', sanitize_text_field( $_POST[ 'call_status' ] ) );
 
 				// Update the existing "Delivery Type" item meta data
 				if( isset( $_POST[ 'item_id_ref' ] ) ){
 					wc_update_order_item_meta( $_POST[ 'item_id_ref' ], 'Delivery Type', $_POST[ 'delivery_type' ] );
 					wc_update_order_item_meta( $_POST[ 'item_id_ref' ], 'Advance Payment', $_POST[ 'advance_payment' ] );
+					wc_update_order_item_meta( $_POST[ 'item_id_ref' ], 'Call Status', $_POST[ 'call_status' ] );
 				}
 			}
 		}
@@ -67,6 +94,7 @@ if( !class_exists( 'Order_Columns_Main' ) ) {
 				$new_columns[ $column_name ] = $column_info;
 				if ( 'order_total' === $column_name ) {
 					$new_columns['delivery_type'] = __( 'Delivery Type', 'woocommerce' );
+					$new_columns['call_status'] = __( 'Call Status', 'woocommerce' );
 					
 				}
 			}
@@ -89,21 +117,36 @@ if( !class_exists( 'Order_Columns_Main' ) ) {
 					echo 'No Data Available!';
 				}
 				else{
-					echo get_post_meta( $post->ID, "_delivery_type", true );
+					echo ucfirst(get_post_meta( $post->ID, "_delivery_type", true ));
 				}
+				break;
+				case 'call_status' :					
+				if((get_post_meta( $post->ID, "_call_status", true )) == NULL){
+					echo 'No Data Available!';
+				}
+				else{
+					echo ucfirst(get_post_meta( $post->ID, "_call_status", true ));
+				}
+				break;
 			}
 
 		}
 
 
+		//Advance payment calculate
 		function calculate_advance_payment( $the_order ) {
 			$getTotal = $the_order->get_total();
-			$updateTotal = $getTotal - get_post_meta($the_order->get_id(), "_advance_payment", true);
+
+			//echo 'Order ID = ' . $the_order->get_id();
+
+			$orderID = $the_order->get_id();
+
+			$updateTotal = $getTotal - get_post_meta($orderID, "_advance_payment", true);
 			$the_order->set_total($updateTotal);
 			$the_order->save();
 		}
 
-		//Display Advance Payment and calculate
+		//Display Advance Payment 
 		function vp_add_sub_total( $the_order ) {
 			global $post, $the_order;
 			if ( empty( $the_order ) || $the_order->get_id() !== $post->ID ) {
@@ -119,7 +162,8 @@ if( !class_exists( 'Order_Columns_Main' ) ) {
 			
 			<?php
 		}
-		 
+
+		
 	}
 }
 
